b53e0acdbaad5540eeb356f74b9dbf2f40b5b7b8
ikbendewilliam <ikbendewilliam@hotmail.com>
2017-11-17 17:30:09 +0100

    bugfix multiplayer

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -5,8 +5,8 @@ import Map from "./Map/Map.class";
 (function () {
     let ctx = document.querySelector("#game").getContext('2d');
 
-    //const socket = io();
-    const socket = io.connect("http://localhost:5000");
+    const socket = io();
+    //const socket = io.connect("http://localhost:5000");
     
     let gamestatemanager = new GameStateManager();
     let mainstate = new MainGameState(ctx, new Map(), socket);
